eee819e88134e4c26b9dbbad2ceed183b05434d2
treyarnold <arnold.trey80@gmail.com>
2019-04-11 18:03:13 -0500

    initial commit

---
+++ number_wizard_console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("Welcome to Number Wizard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
