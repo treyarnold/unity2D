1f95cd3bd833944537ece2771b1006e4e73adf02
Trey Arnold <arnold.trey80@gmail.com>
2019-04-15 14:59:59 -0500

    inital commit on blockbreaker. game working with sound effects

---
+++ blockBreaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+  [SerializeField] float screenWidthInUnits;
+  [SerializeField] float paddleMinX;
+  [SerializeField] float paddleMinY;
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+    Vector2 paddlePos = new Vector2 (transform.position.x, transform.position.y);
+    paddlePos.x = Mathf.Clamp(mousePosInUnits, paddleMinX, paddleMinY);
+    transform.position = paddlePos;
+  }
+}
